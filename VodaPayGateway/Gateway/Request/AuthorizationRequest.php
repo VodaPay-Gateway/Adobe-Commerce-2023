3796bf0d73cc83c580c8327c93053f70b073a160
vfsLihle <lihle.zwane@vcontractor.co.za>
2023-06-28 13:39:50 +0200

    logging

--- VodaPayGateway/Gateway/Request/AuthorizationRequest.php
+++ VodaPayGateway/Gateway/Request/AuthorizationRequest.php
@@ -17,6 +17,7 @@ use Magento\Payment\Gateway\ConfigInterface;
 use Magento\Payment\Gateway\Data\PaymentDataObjectInterface;
 use Magento\Payment\Gateway\Request\BuilderInterface;
 use Magento\Payment\Model\Method\Logger;
+use Magento\Payment\Gateway\Helper;
 
 class AuthorizationRequest implements BuilderInterface
 {
@@ -50,54 +51,62 @@ class AuthorizationRequest implements BuilderInterface
      */
     public function build(array $buildSubject)
     {
-        $this->logger->debug('Logger test : '. json_decode($buildSubject));
-        // if (!isset($buildSubject['payment'])
-        //     || !$buildSubject['payment'] instanceof PaymentDataObjectInterface
-        // ) {
-        //     throw new \InvalidArgumentException('Payment data object should be provided');
-        // }
+		$writer = new \Zend_Log_Writer_Stream(BP . '/var/log/newfile.log');
+		$Zlogger = new \Zend_Log();
+		$Zlogger->addWriter($writer);
+		//$Zlogger->info('text message');
+		//$Zlogger->info('Array Log'.print_r($myArray, true));
 
+        $Zlogger->info('Logger test : '. json_encode($buildSubject));
+		$paymentObj = Helper\SubjectReader::readPayment($subject);
+        if (!isset($buildSubject['payment'])
+            || !$buildSubject['payment'] instanceof PaymentDataObjectInterface
+        ) {
+			$Zlogger->info('Logger obj fail : '. json_encode($buildSubject['payment']));
+            throw new \InvalidArgumentException('Payment data object should be provided');
+        }
 
+		$payloadVodapay = new \VodaPayGatewayClient\Model\VodaPayGatewayPayment();
 
-        ///** @var PaymentDataObjectInterface $payment */
-        // $payment = $buildSubject['payment'];
-        // $order = $payment->getOrder();
-        // $address = $order->getShippingAddress();
-
-        // $amount = intval($this->getTotalAmount( $order ) * 100);// The amount must be in cents.
+		$Zlogger->info('Logger obj fail : '. json_encode($buildSubject['payment']));
+		try
+		{
+				/** @var PaymentDataObjectInterface $payment */
+				$payment = $buildSubject['payment'];
+				$order = $payment->getOrder();
+				$address = $order->getShippingAddress();
 		
+				$amount = intval($this->getTotalAmount( $order ) * 100);// The amount must be in cents.
+				//$this->logger->debug('order : '. json_encode($order));
+				$amount = intval($this->getTotalAmount( $order ) * 100);// The amount must be in cents.
+				$payloadVodapay->setAmount($amount);
+				$rlength = 10;
+				$retrievalReference =   substr(
+					str_shuffle(str_repeat($x='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+						ceil($rlength/strlen($x)) )),1,32
+				);
+				$retrievalReference = str_pad($order->get_order_number() ?? " ", 12, $retrievalReference, STR_PAD_LEFT);
+				$payloadVodapay->setTraceId(strval($retrievalReference));
+				$payloadVodapay->setEchoData(json_encode(['order_id'=>$order->getRealOrderId()]));
+				$additionData = new \VodaPayGatewayClient\Model\PaymentIntentAdditionalDataModel;
+				$styling = new \VodaPayGatewayClient\Model\Styling;
+				$styling->setLogoUrl("");
+				$styling->setBannerUrl("");
+				$payloadVodapay->setStyling($styling);	
 
-		// $payloadVodapay = new \VodaPayGatewayClient\Model\VodaPayGatewayPayment();
+				$peripheryData = new \VodaPayGatewayClient\Model\Notifications;
+				$peripheryData->setCallbackUrl('https://vodapay.magento.com/vodapaygateway/redirect/success');
+				//$peripheryData->setNotificationUrl();
+				$payloadVodapay->setNotifications($peripheryData);
 
-		// //$this->logger->debug('order : '. json_encode($order));
+		}
+		catch (Exception $e) {
 		
-		// $amount = intval($this->getTotalAmount( $order ) * 100);// The amount must be in cents.
-		// $payloadVodapay->setAmount($amount);
-
-
-		// $rlength = 10;
-		// $retrievalReference =   substr(
-		// 	str_shuffle(str_repeat($x='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
-		// 		ceil($rlength/strlen($x)) )),1,32
-		// );
-		// $retrievalReference = str_pad($order->get_order_number() ?? " ", 12, $retrievalReference, STR_PAD_LEFT);
-		// $payloadVodapay->setTraceId(strval($retrievalReference));
-
-
-		// $payloadVodapay->setEchoData(json_encode(['order_id'=>$order->getRealOrderId()]));
-
-
-		// $additionData = new \VodaPayGatewayClient\Model\PaymentIntentAdditionalDataModel;
-
-		// $styling = new \VodaPayGatewayClient\Model\Styling;
-		// $styling->setLogoUrl("");
-		// $styling->setBannerUrl("");
-		// $payloadVodapay->setStyling($styling);	
-
-		// $peripheryData = new \VodaPayGatewayClient\Model\Notifications;
-		// $peripheryData->setCallbackUrl('https://vodapay.magento.com/vodapaygateway/redirect/success');
-		// //$peripheryData->setNotificationUrl();
-		// $payloadVodapay->setNotifications($peripheryData);
+			$writer = new \Zend_Log_Writer_Stream(BP . '/var/log/newExceptionfile.log');
+			$Zlogger = new \Zend_Log();
+			$Zlogger->addWriter($writer);
+			$Zlogger->info('Logger Exception : '. json_encode(array('msg'=>$e->getMessage())));
+		} 
 
         return strval($payloadVodapay);
     }
